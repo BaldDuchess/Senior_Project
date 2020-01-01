42f1bee8d68f558b6c477273662c75d682322ead
jasonRsheetz <37478927+jasonRsheetz@users.noreply.github.com>
2019-12-31 19:34:54 -0700

    Add files via upload

--- Senior Project/Fuel_Injection/Debug/main.d
+++ Senior Project/Fuel_Injection/Debug/main.d
@@ -1,23 +1,21 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdint.h
-main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_stdint40.h
-main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/stdint.h
-main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h
-main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h
-main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h
-main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_stdint.h
-main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h
 main.obj: ../tm4c123gh6pm.h
 
 ../main.c: 
-C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdint.h: 
-C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_stdint40.h: 
-C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/stdint.h: 
-C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h: 
-C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h: 
-C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h: 
-C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_stdint.h: 
-C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h: 
 ../tm4c123gh6pm.h: 
--- Senior Project/Fuel_Injection/main.c
+++ Senior Project/Fuel_Injection/main.c
@@ -4,13 +4,6 @@
 
 //defines
 //PWM
-#define PWM_0_GENA_ACTCMPAD_ONE 0x000000C0  // Set the output signal to 1
-#define PWM_0_GENA_ACTLOAD_ZERO 0x00000008  // Set the output signal to 0
-#define PWM_0_GENB_ACTCMPBD_ONE 0x00000C00  // Set the output signal to 1
-#define PWM_0_GENB_ACTLOAD_ZERO 0x00000008  // Set the output signal to 0
-#define SYSCTL_RCC_USEPWMDIV    0x00100000  // Enable PWM Clock Divisor
-#define SYSCTL_RCC_PWMDIV_M     0x000E0000  // PWM Unit Clock Divisor
-#define SYSCTL_RCC_PWMDIV_2     0x00000000  // /2
 
 
 //void DisableInterrupts(void); // Disable interrupts
@@ -24,58 +17,117 @@ void WaitForInterrupt(void);  // low power mode
 volatile uint32_t FallingEdges = 0;
 
 //interrupts
-void EdgeCounter_Init(void){
-  SYSCTL_RCGCGPIO_R |= 0x00000020; // (a) activate clock for port F
-  FallingEdges = 0;             // (b) initialize counter
-  GPIO_PORTF_DIR_R &= ~0x10;    // (c) make PF4 in (built-in button)
-  GPIO_PORTF_AFSEL_R &= ~0x10;  //     disable alt funct on PF4
-  GPIO_PORTF_DEN_R |= 0x10;     //     enable digital I/O on PF4
-  GPIO_PORTF_PCTL_R &= ~0x000F0000; // configure PF4 as GPIO
-  GPIO_PORTF_AMSEL_R = 0;       //     disable analog functionality on PF
-  GPIO_PORTF_PUR_R |= 0x10;     //     enable weak pull-up on PF4
-  GPIO_PORTF_IS_R &= ~0x10;     // (d) PF4 is edge-sensitive
-  GPIO_PORTF_IBE_R &= ~0x10;    //     PF4 is not both edges
-  GPIO_PORTF_IEV_R &= ~0x10;    //     PF4 falling edge event
-  GPIO_PORTF_ICR_R = 0x10;      // (e) clear flag4
-  GPIO_PORTF_IM_R |= 0x10;      // (f) arm interrupt on PF4 *** No IME bit as mentioned in Book ***
-  NVIC_PRI7_R = (NVIC_PRI7_R&0xFF00FFFF)|0x00A00000; // (g) priority 5
-  NVIC_EN0_R = 0x40000000;      // (h) enable interrupt 30 in NVIC
-  EnableInterrupts();           // (i) Clears the I bit
+void PortF_Interrupt_Init(void){
+  SYSCTL_RCGCGPIO_R |= 0x00000020; //activate clock for port F
+  FallingEdges = 0;             //initialize counter
+  GPIO_PORTF_DIR_R &= ~0x10;    //make PF4 in (built-in button)
+  GPIO_PORTF_AFSEL_R &= ~0x10;  //disable alt funct on PF4
+  GPIO_PORTF_DEN_R |= 0x10;     //enable digital I/O on PF4
+  GPIO_PORTF_PCTL_R &= ~0x000F0000; //configure PF4 as GPIO
+  GPIO_PORTF_AMSEL_R = 0;       //disable analog functionality on PF
+  GPIO_PORTF_PUR_R |= 0x10;     //enable weak pull-up on PF4
+  GPIO_PORTF_IS_R &= ~0x10;     //PF4 is edge-sensitive
+  GPIO_PORTF_IBE_R &= ~0x10;    //PF4 is not both edges
+  GPIO_PORTF_IEV_R = 0x01;    //PF4 rising edge event
+  GPIO_PORTF_ICR_R = 0x10;      //clear flag4
+  GPIO_PORTF_IM_R |= 0x10;      //arm interrupt on PF4 *** No IME bit as mentioned in Book ***
+  NVIC_PRI7_R = (NVIC_PRI7_R&0xFF00FFFF)|0x00A00000; //priority 5
+  NVIC_EN0_R = 0x40000000;      //enable interrupt 30 in NVIC
+  EnableInterrupts();           //Clears the I bit
 }
 void GPIOPortF_Handler(void){
   GPIO_PORTF_ICR_R = 0x10;      // acknowledge flag4
-  FallingEdges = FallingEdges + 1;
+  FallingEdges = FallingEdges + 1;  //increment counter
+  GPIO_PORTF_DATA_R = 2;      /* turn on red LED */
+
+  //initialize timer0
+  TIMER0_TAMATCHR_R = 0x00;           //set interrupt compare to 0
+  TIMER0_TAILR_R = 0x000FFFFF; //Timer A interval load value register
+  TIMER0_CTL_R |= 0x01;           //enable Timer A after initialization
 }
 
-
-//PWM
-void PWM0A_Init(uint16_t period, uint16_t duty){
-  SYSCTL_RCGCPWM_R |= 0x01;             // 1) activate PWM0
-  SYSCTL_RCGCGPIO_R |= 0x02;            // 2) activate port B
-  while((SYSCTL_PRGPIO_R&0x02) == 0){};
-  GPIO_PORTB_AFSEL_R |= 0x40;           // enable alt funct on PB6
-  GPIO_PORTB_PCTL_R &= ~0x0F000000;     // configure PB6 as PWM0
-  GPIO_PORTB_PCTL_R |= 0x04000000;
-  GPIO_PORTB_AMSEL_R &= ~0x40;          // disable analog functionality on PB6
-  GPIO_PORTB_DEN_R |= 0x40;             // enable digital I/O on PB6
-  SYSCTL_RCC_R = 0x00100000 |           // 3) use PWM divider
-      (SYSCTL_RCC_R & (~0x000E0000));   //    configure for /2 divider
-  PWM0_0_CTL_R = 0;                     // 4) re-loading down-counting mode
-  PWM0_0_GENA_R = 0xC8;                 // low on LOAD, high on CMPA down
-  // PB6 goes low on LOAD
-  // PB6 goes high on CMPA down
-  PWM0_0_LOAD_R = period - 1;           // 5) cycles needed to count down to 0
-  PWM0_0_CMPA_R = duty - 1;             // 6) count value when output rises
-  PWM0_0_CTL_R |= 0x00000001;           // 7) start PWM0
-  PWM0_ENABLE_R |= 0x00000001;          // enable PB6/M0PWM0
+void Timer0A_Handler(void)
+{
+    TIMER0_ICR_R = 0x11;        //acknowledge flag
+    GPIO_PORTF_DATA_R = 0;      //turn off red LED
+}
+void GPIO_Init(void)
+{
+    SYSCTL_RCGC2_R |= 0x00000020;   //enable clock to GPIOF at clock gating control register
+    GPIO_PORTF_DIR_R |= 0x0E;        //enable the GPIO pins for the LED (PF3, 2 1) as output
+    GPIO_PORTF_DEN_R |= 0x0E;        //enable the GPIO pins for digital function
 }
 
+void Timer_Init(void)
+{
+    SYSCTL_RCGC1_R |= SYSCTL_RCGC1_TIMER0; //activate timer0
+    TIMER0_CTL_R &= ~0x00000001;     //disable timer0A during setup
+    TIMER0_CFG_R = 0x00;            //32-bit option
+    TIMER0_TAMR_R = 0x01;           //one-shot mode and down-counter
+    TIMER0_TAMR_R |= 0x20;          //enable interrupts for one-shot mode
+    TIMER0_IMR_R |= 0x1F;           // enable compare match interrupt
+    NVIC_PRI4_R = (NVIC_PRI4_R&0x00FFFFFF)|0x40000000; // 8) priority 2
+    NVIC_EN0_R = 0x80000;      //enable interrupt 19 in NVIC
+}
 
 //debug code
 int main(void){
-  EdgeCounter_Init();
-  PWM0A_Init(40000, 30000);// initialize GPIO Port F interrupt
+  Timer_Init();
+  PortF_Interrupt_Init();
+  GPIO_Init();
+  
   while(1){
-    //WaitForInterrupt();
+
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
