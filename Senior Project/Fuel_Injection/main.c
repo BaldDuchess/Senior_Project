c9763b66c60a8916ded4765ddd490dd7197f3eef
jasonRsheetz <37478927+jasonRsheetz@users.noreply.github.com>
2020-01-09 22:33:31 -0800

    Update main.c

--- Senior Project/Fuel_Injection/main.c
+++ Senior Project/Fuel_Injection/main.c
@@ -13,7 +13,6 @@ void WaitForInterrupt(void);  // low power mode
 
 // global variable visible in Watch window of debugger
 // increments at least once per button press
-volatile uint64_t FallingEdges = 0;
 volatile uint32_t crank_period_capture = 0;
 volatile uint16_t crank_period = 0;
 volatile uint32_t half_crank_period = 0;
@@ -21,9 +20,6 @@ char TDC = 1;
 volatile uint32_t advance = 0;
 volatile uint16_t spark_duration = 0x1;
 volatile uint16_t fuel_duration = 0x1;
-volatile uint8_t firing_order[] = {1,4,3,2};
-volatile uint8_t cyl_to_fuel = 1;
-volatile uint8_t counter = 0;
 
 static uint16_t front_cyl_bank_spark = 0x01;
 static uint16_t rear_cyl_bank_spark = 0x02;
@@ -32,10 +28,13 @@ static uint16_t cyl_2_fuel = 0x08;
 static uint16_t cyl_3_fuel = 0x10;
 static uint16_t cyl_4_fuel = 0x20;
 
+volatile uint8_t cyl_to_fuel_front = 1;
+volatile uint8_t cyl_to_fuel_rear = 2;
+
+
 //interrupts
 void PortF_Interrupt_Init(void){
   SYSCTL_RCGCGPIO_R |= 0x00000020; //activate clock for port F
-  FallingEdges = 0;             //initialize counter
   GPIO_PORTF_DIR_R &= ~0x10;    //make PF4 in (built-in button)
   GPIO_PORTF_AFSEL_R &= ~0x10;  //disable alt funct on PF4
   GPIO_PORTF_DEN_R |= 0x10;     //enable digital I/O on PF4
@@ -57,13 +56,9 @@ void PortF_Interrupt_Init(void){
 //Returns: none
 void GPIOPortF_Handler(void){
 
-        FallingEdges = 0;
   TDC = 1;   //if this interrupt has fired piston #1 is coming up on TDC
 
   GPIO_PORTF_ICR_R = 0x10;      // acknowledge flag4
-
-  counter++;
-  cyl_to_fuel = firing_order[counter]
 
   //re-start timer2 - determines crank period (RPMs)
   crank_period_capture = TIMER2_TAV_R;  //capture timer2's value
@@ -85,9 +80,8 @@ void GPIOPortF_Handler(void){
 void Timer0A_Handler(void)
 {
     TIMER0_ICR_R = 0x11;        //acknowledge flag
-    GPIO_PORTF_DATA_R = 0;      //turn off LEDs
+    GPIO_PORTB_DATA_R &= ~0x3;      //shut off spark
 }
-
 //Purpose: interrupt that turns off the fuel pulse.
 //Parameters: none
 //Returns: none
@@ -95,6 +89,7 @@ void Timer1A_Handler(void)
 {
     TIMER1_ICR_R = 0x11;        //acknowledge flag
 
+    GPIO_PORTB_DATA_R &= ~0xFC;      //shut off fuel
 }
 
 //Purpose: starts fuel and spark pulses at the appropriate advance times, also re-arms itself for the BDC timing
@@ -102,11 +97,26 @@ void Timer1A_Handler(void)
 //Returns: none
 void Timer3A_Handler(void)
 {
+
+
     if(TDC)
     {
         TIMER3_ICR_R = 0x11;        //acknowledge flag
-        GPIO_PORTB_DATA_R = front_cyl_bank_spark;      /* turn on red LED */
-        GPIO_PORTB_DATA_R
+        GPIO_PORTB_DATA_R |= front_cyl_bank_spark;      //start the spark
+
+        switch(cyl_to_fuel_front)
+        {
+        case    1:
+            GPIO_PORTB_DATA_R |= cyl_1_fuel;
+            cyl_to_fuel_front = 3;
+            break;
+        case    3:
+            GPIO_PORTB_DATA_R |= cyl_3_fuel;
+            cyl_to_fuel_front = 1;
+            break;
+        default:
+            GPIO_PORTF_DATA_R |= 0x0;   //do nothing
+        }
 
         //re-start timer0 - controls duration of spark signal
         TIMER0_TAMATCHR_R = 0x00;           //set interrupt compare to 0
@@ -126,8 +136,22 @@ void Timer3A_Handler(void)
     }else
     {
         TIMER3_ICR_R = 0x11;        //acknowledge flag
-        GPIO_PORTB_DATA_R = rear_cyl_bank_spark;      /* turn on blue LED */
-        GPIO_PORTB_DATA_R
+        GPIO_PORTB_DATA_R |= rear_cyl_bank_spark;      //start the spark
+
+        switch(cyl_to_fuel_rear)
+        {
+        case    2:
+            GPIO_PORTB_DATA_R |= cyl_2_fuel;
+            cyl_to_fuel_rear = 4;
+            break;
+        case    4:
+            GPIO_PORTB_DATA_R |= cyl_4_fuel;
+            cyl_to_fuel_rear = 2;
+            break;
+        default:
+            GPIO_PORTF_DATA_R |= 0x0;   //do nothing
+        }
+
         //re-start timer0 - controls duration of spark signal
         TIMER0_TAMATCHR_R = 0x00;           //set interrupt compare to 0
         TIMER0_TAILR_R = spark_duration; //Timer A interval load value register
@@ -147,7 +171,7 @@ void GPIO_Init(void)
     GPIO_PORTF_DEN_R |= 0xEF;        //enable the GPIO pins for digital function
 
     //PortB is used for sending spark and fuel pulses
-    SYSCTL_RCGC2_R |= 0x2;   //enable clock to GPIOF at clock gating control register
+    SYSCTL_RCGC2_R |= 0x2;   //enable clock to GPIOB at clock gating control register
     GPIO_PORTB_DIR_R |= 0xFF;        //enable the GPIO pins on port F: PF4 is input for crank signal, all others are outputs for fuel or spark pulses
     GPIO_PORTB_AFSEL_R &= ~0XFF;    //disable alternate port functions
     GPIO_PORTB_DEN_R |= 0xFF;        //enable the GPIO pins for digital function
@@ -206,13 +230,10 @@ int main(void){
   PortF_Interrupt_Init();
   GPIO_Init();
   while(1){
-      if(crank_period_capture > 33000)
-      {
-          spark_duration = 0xFFFF;
-      }
-      else
-      {
+
           spark_duration = 0x1;
-      }
+          fuel_duration = crank_period_capture/800;
+
+
   }
 }
